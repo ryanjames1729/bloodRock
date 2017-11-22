1d7a6e123285fe0436e3d68548442c64f43e9435
Ryan James <ryanjames1729@gmail.com>
2017-11-22 07:27:45 -0600

    Update code.js

--- code.js
+++ code.js
@@ -9,5 +9,12 @@ var x = setInterval(function(){
   var seconds = Math.abs(Math.floor((distance % (1000 * 60)) / 1000));
 
   document.getElementById("timer").innerHTML = "Race Time: " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-  document.getElementById("clock").innerHTML = "Current Time: " + now.getHours() + ":" + now.getMinutes();
+  document.getElementById("clock").innerHTML = "Current Time: " + now.getHours() + ":" + digits(now.getMinutes());
 }, 1000);
+
+function digits(time){
+  if (time < 10){
+    return "0" + time;
+  }
+  return time;
+}
